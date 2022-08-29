fe67a64d89a295c875763413bb22c1411c1f6617
Lucas-Bispo <lukaobispo@gmail.com>
2022-08-29 13:21:56 -0300

    :construction: Trabalho em progresso.

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ sudo apt-get update
 #Instalar NGINX
 sudo apt-get install nginx
 #Inicializar NGINX
+service nginx start
+#Verificar status de service NGINX
+service nginx status
+#intalar PHP-FPM que é apropriado para o nginx e intalar mysql
+sudo apt-get install php-fpm php-mysql 
+#Ir nos arquivos de configuração do nginx
+cd /etc/nginx
+#Congurar nginx para entender o PHP
+cd sites-available
+#Configurar o arquivo default
+vim default
