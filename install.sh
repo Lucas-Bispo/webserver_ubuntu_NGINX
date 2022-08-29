19ef713c0078f5e86c0f87464288b65452c7245f
Lucas-Bispo <lukaobispo@gmail.com>
2022-08-29 12:32:01 -0300

    :tada:Comece um projeto.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Instalando WebServer avançado
+#Instalando servidor web avançado completo. NGINX + PHP-FPM + MARIADB + PHPMYADMIN
+#Fazer login como super usuario
+sudo su
+#Atualizar repository
+sudo apt-get update
+#Instalar NGINX
+sudo apt-get install nginx
+#Inicializar NGINX
