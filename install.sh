314c7f330fecc1ed7435850dd5b385deac942675
Lucas-Bispo <lukaobispo@gmail.com>
2022-08-29 15:17:10 -0300

    :construction: Trabalho em progresso.

--- install.sh
+++ install.sh
@@ -1,20 +1,30 @@
-#Instalando WebServer avançado
-#Instalando servidor web avançado completo. NGINX + PHP-FPM + MARIADB + PHPMYADMIN
-#Fazer login como super usuario
+# Instalando WebServer avançado
+# Instalando servidor web avançado completo. NGINX + PHP-FPM + MARIADB + PHPMYADMIN
+# Fazer login como super usuario
 sudo su
-#Atualizar repository
+# Atualizar repository
 sudo apt-get update
-#Instalar NGINX
+# Instalar NGINX
 sudo apt-get install nginx
-#Inicializar NGINX
+# Inicializar NGINX
 service nginx start
-#Verificar status de service NGINX
+# Verificar status de service NGINX
 service nginx status
-#intalar PHP-FPM que é apropriado para o nginx e intalar mysql
+# intalar PHP-FPM que é apropriado para o nginx e intalar mysql
 sudo apt-get install php-fpm php-mysql 
-#Ir nos arquivos de configuração do nginx
+# Ir nos arquivos de configuração do nginx
 cd /etc/nginx
-#Congurar nginx para entender o PHP
+# Congurar nginx para entender o PHP
 cd sites-available
-#Configurar o arquivo default
+# Configurar o arquivo default
 vim default
+# Retirar # de comentario da linha da linha 56, 57, 60, 63
+# adicionar index.php na linha 44 depois de "index.php"
+# Reinicar service NGINX
+service nginx restart
+# Criar index.php 
+cd /var/www/html/
+vim index.php
+# <?php
+# php info();
+# ?> 
